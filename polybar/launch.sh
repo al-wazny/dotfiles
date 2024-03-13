f0060d5389310f20b294bc2cd694af427dbe07a1
Ali Al-Wazny <ali@pop-os.localdomain>
2024-03-13 10:25:15 +0100

    feat: Add all the other needed dotfiles

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+# If all your bars have ipc enabled, you can use 
+# polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+echo "---" | tee -a /tmp/polybar1.log
+
+polybar --config=~/.config/polybar/config.ini example 2>&1 | tee -a /tmp/polybar1.log & disown
+
+echo "Bars launched..."
